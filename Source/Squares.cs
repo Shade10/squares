f030ff10be436ef28c4d5aad46e3644ccf6115ed
Marcin Kass <marcin.kass10@gmail.com>
2017-01-22 20:10:11 +0100

    Add project files.

---
+++ Source/Squares.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace squares
+{
+    public class Squares
+    {
+        private int value;
+        private int sum;
+        private int result;
+
+        public Squares(int value)
+        {
+            if (value < 0)
+                throw new ArgumentOutOfRangeException();
+            this.value = value;
+        }
+
+        public int SquareOfSums()
+        {
+            for (int i = 1; i <= value; i++)
+                sum += i;
+            return (int)Math.Pow(sum, 2);
+        }
+        public int SumOfSquares()
+        {
+            for (int i = 1; i <= value; i++)
+                result += (int)Math.Pow(i, 2);
+            return result;
+        }
+        public int DifferenceOfSquares()
+        {
+            result = SquareOfSums() - SumOfSquares();   
+            return result;
+        }
+
+        
+    }
+}
